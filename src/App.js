f9adebcf9d403137d4cb07b81c1f9409e9c28a7a
auliaramadhan <35895827+auliaramadhan@users.noreply.github.com>
2018-09-19 13:08:07 +0700

    tambahin redux

--- src/App.js
+++ src/App.js
@@ -16,19 +16,10 @@ class App extends Component {
   }
 
   getTodos() {
-    $.ajax({
-      url: 'https://jsonplaceholder.typicode.com/todos',
-      dataType: 'json',
-      cache: false,
-      success: function (data) {
-        this.setState({ todos: data }, function () {
-          console.log(this.state);
-        });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.log(err);
-      }
-    });
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res =>  res.json())
+    .then(data => this.setState({todos : data}));
+
   }
 
   getProjects() {
